0cf59195f3c27459f0fb956cd6890fddede7eaf1
Timothy.Esposito <esposito_timothy@yahoo.com>
2023-04-16 21:25:08 -0700

    Fixing Navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
             {/* Close icon */}
             <div className='flex justify-end p-12'>
               <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                <img alt='close-icon' src='../assets/close-icon.svg' />
+                <img alt='close-icon' src='assets/close-icon.svg' />
               </button>
             </div>
             {/* Menu Items */}
